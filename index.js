241c82d76fbfbe2113b6fa63f9fcabf24b12bdf4
Jon Kaplon <jon.kaplon1@gmail.com>
2021-12-13 07:30:07 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -19,15 +19,6 @@ function moveDodgerRight() {
     }
 }
 
-// function moveDodgerRight() {
-//     const rightNumbers = dodger.style.right.replace("px", "");
-//     const right = parseInt(rightNumbers, 10);
-
-//     if (right > 0) {
-//         dodger.style.right = `${right + 1}px`;
-//     }
-// }
-
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
         moveDodgerLeft();
